c52eebe49e12ad72e97fc2df7c7edeefdce8060b
Anatoly <anatoly.kalugin4@yandex.ru>
2022-10-30 12:09:44 +0300

    fix: избавиться от N+1 запросов

--- src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
+++ src/main/java/ru/practicum/shareit/request/service/ItemRequestServiceImpl.java
@@ -19,7 +19,10 @@ import ru.practicum.shareit.user.dto.UserMapper;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 @Service
@@ -75,13 +78,11 @@ public class ItemRequestServiceImpl implements ItemRequestService {
     public List<ItemRequestDto> getRequests(Long authorId) {
         log.info("Получен запрос на получение реквестов");
         if (userService.getUserById(authorId) != null) {
+            Map<Long, List<Item>> requestItemMap = extractItemsToRequests();
             return itemRequestRepository.getByRequestorIdOrderByCreatedAsc(authorId)
                     .stream()
-                    .map(itemRequest -> {
-                        List<Item> items = itemRepository.getByRequestId(itemRequest.getId(),
-                                Sort.by("id").descending());
-                        return ItemRequestDtoMapper.mapTo(itemRequest, items);
-                    })
+                    .map(itemRequest
+                            -> ItemRequestDtoMapper.mapTo(itemRequest, requestItemMap.get(itemRequest.getId())))
                     .collect(Collectors.toList());
         } else {
             log.warn("Ошибка - неправильный айди юзера");
@@ -93,15 +94,13 @@ public class ItemRequestServiceImpl implements ItemRequestService {
     public List<ItemRequestDto> getOthersRequests(Long userId, int index, int quantity) {
         log.info("Получен запрос на получение всех реквестов");
         if (index >= 0 && quantity > 0) {
+            Map<Long, List<Item>> requestItemMap = extractItemsToRequests();
             return itemRequestRepository.getByRequestorIdNot(userId,
                             PageRequest.of(index / quantity, quantity,
                                     Sort.by("created").descending()))
                     .stream()
-                    .map(itemRequest -> {
-                        List<Item> items = itemRepository.getByRequestId(itemRequest.getId(),
-                                Sort.by("id").descending());
-                        return ItemRequestDtoMapper.mapTo(itemRequest, items);
-                    })
+                    .map(itemRequest
+                            -> ItemRequestDtoMapper.mapTo(itemRequest, requestItemMap.get(itemRequest.getId())))
                     .collect(Collectors.toList());
         } else {
             log.warn("Ошибка - некорректный запрос");
@@ -109,4 +108,20 @@ public class ItemRequestServiceImpl implements ItemRequestService {
         }
     }
 
+    private Map<Long, List<Item>> extractItemsToRequests() {
+        Map<Long, List<Item>> requestItemMap = new HashMap<>();
+        List<Item> itemList = itemRepository.findAll();
+        List<ItemRequest> itemRequestList = itemRequestRepository.findAll();
+        for (ItemRequest itemRequest : itemRequestList) {
+            List<Item> itemsToAdd = new ArrayList<>();
+            for (Item item : itemList) {
+                if (item.getRequest() != null &&
+                        item.getRequest().getId().equals(itemRequest.getId())) {
+                    itemsToAdd.add(item);
+                }
+            }
+            requestItemMap.put(itemRequest.getId(), itemsToAdd);
+        }
+        return requestItemMap;
+    }
 }
